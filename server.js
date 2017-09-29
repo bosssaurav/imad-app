83116049db65952c33784773dfc188dddc4d2a70
bosssaurav <cool.saurav10@gmail.com>
2017-09-29 11:56:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.get('/ui/madi.png', function (req, res) {
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
-  console.log(path);
+  console.log("ABCDEF:"+path);
 });
