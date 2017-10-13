ca4c2bd61a1f9feda36e30157196668f788e366b
bosssaurav <cool.saurav10@gmail.com>
2017-10-14 00:42:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'login/', 'login.php'));
+  res.sendFile(path.join(__dirname, 'login/', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
